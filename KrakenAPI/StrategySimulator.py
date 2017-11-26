d66b145c99dd119b394191e95834226f90d0495b
Arnaud Melin <arnaudmelin@hotmail.com>
2017-11-26 22:28:49 +0100

    Small test for parameters optimization and auto-, lagged-correlation

--- KrakenAPI/StrategySimulator.py
+++ KrakenAPI/StrategySimulator.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import pprint as pp
 from Indicators import Indicators
 from Helpers import Helpers  # same folder module
 #Indicators = Indicators()
@@ -18,7 +19,7 @@ class StrategySimulator:
     def __init__(self):
         self.helpers = Helpers()
 
-    def test_strategy(self, indicator, data=None):
+    def test_strategy(self, indicator, params=None, data=None):
         # DATA
         if data is None:
             stock = pd.read_csv('../Data/bitcoin_histo.csv', sep=';')
@@ -33,7 +34,10 @@ class StrategySimulator:
         # COMPUTATIONS
 
         if indicator == 'macd':
-            macd, macd_ema9 = Indicators.macd(pxlast, long_span=26, short_span=12, smoothing_factor=9)
+            ss = params[0]
+            ls = params[1]
+            sf = params[2]
+            macd, macd_ema9 = Indicators.macd(pxlast, short_span=ss, long_span=ls, smoothing_factor=sf)
             returns = pxlast.pct_change(1)
             invested = Helpers.indicators_to_investment(indicator_name='macd', data=(macd, macd_ema9))
         elif indicator == 'ema':
@@ -47,6 +51,8 @@ class StrategySimulator:
 
         # PROFITABILITY
         total_profit, strat_profit = self.profitability(returns, invested)
+
+        # return strat_profit.iloc[-1].values[0]
 
         # PLOT
         data_plot = pd.concat([total_profit, strat_profit], axis=1)
@@ -74,8 +80,6 @@ class StrategySimulator:
         # total_profit.plot()
         # strat_profit.plot()
 
-        # Pour voir le graphe s'afficher, mettre un breakpoint devant 'input(...)' et lancer en debug - à fixer
-        input('...')
 
     def profitability(self, returns_mat, invested_mat):
         # Check returns matrix has no NaN - if so - replace by 0
@@ -95,15 +99,46 @@ class StrategySimulator:
 if __name__ == '__main__':
     s = StrategySimulator()
     ccy_pairs = [
+        'XXBTZEUR',
+        # 'XXBTZUSD',
         'XETHZEUR',
-        # 'XETCZEUR',
-        # 'XXRPZEUR'
+        # 'XETHZUSD',
+        'XETCZEUR',
+        # 'XETCZUSD',
+        'XXRPZEUR',
+        # 'XXRPZUSD',
     ]
-    since_date = datetime.strptime('01-01-2017', '%d-%m-%Y')
-    interval = 60
+    since_date = datetime.strptime('25-06-2017', '%d-%m-%Y')
+    interval = 1440
     value_type = 'close'
     data_df = s.helpers.mongo_to_df(ccy_pairs=ccy_pairs, since_date=since_date, interval=interval,
                                     value_type=value_type)
+
+    # CORRELATION
+    data_df_returns = data_df.pct_change(1)
+    correl_matrix = data_df_returns.corr()
+    pp.pprint(correl_matrix)
+
+    data_df_returns = data_df_returns.dropna().reset_index()
+    del data_df_returns['index']
+    ref = 'XXBTZEUR'
+    data_ref_returns = data_df_returns[ref]
+    ref_name = ref + ' - ref'
+    data_ref_returns.name = ref_name
+
+    # pd.concat([data_ref_returns, data_df_returns], ignore_index=True, axis=1)
+    auto_correl_results = pd.DataFrame()
+    all_data = pd.concat([data_ref_returns, data_df_returns], axis=1)
+    for lag in range(0, 100, 1):
+        if lag > 0:
+            all_data[ref_name] = all_data[ref_name].shift(1)
+        auto_correl_results = pd.concat([auto_correl_results, all_data.corr()[ref_name]], axis=1)
+
+    auto_correl_results = auto_correl_results.transpose().reset_index()
+    del auto_correl_results['index']
+    auto_correl_results.plot()
+    plt.show(block=True)
+
     # data_df.plot()
     # plt.show(block=True)
 
@@ -116,4 +151,22 @@ if __name__ == '__main__':
     # value = 'close'
     # data = s.helpers.mongo_to_dict_list(table='ohlc_%s' % str(interval), query=query, output_value=value)
     # data_df = s.helpers.tuple_list_to_dframe(data['close'])
-    s.test_strategy(indicator='macd_rsi', data=data_df)
+
+
+
+
+    # OPTIMIZATION TEST
+    # i = 3
+    # j = 10
+    # k = 3
+    # s.test_strategy(indicator='macd', params=(i, j, k), data=data_df)
+    #
+    # tot_returns = np.zeros((i, j, k))
+    # for ss in range(1, i, 2):
+    #     for ls in range(2, j, 4):
+    #         for sf in range(1, k, 2):
+    #             pp.pprint((ss, ls, sf))
+    #             tot_returns[ss, ls, sf] = s.test_strategy(indicator='macd', params=(ss, ls, sf), data=data_df)
+    #
+    # pp.pprint(tot_returns)
+    # pp.pprint(np.unravel_index(tot_returns.argmax(), tot_returns.shape))
--- acquisition/TradeDataDownloader.py
+++ acquisition/TradeDataDownloader.py
@@ -19,180 +19,180 @@ class TradeDataDownloader:
         self.acquisition_helpers = AcquisitionHelpers()
         self.currency_pairs = {
             # Maintain manually ...
-            # 'BCHEUR': {
-            #     'download_data': True
-            # },
-            # 'BCHUSD': {
-            #     'download_data': True
-            # },
-            # 'BCHXBT': {
-            #     'download_data': True
-            # },
-            # 'DASHEUR': {
-            #     'download_data': False
-            # },
-            # 'DASHUSD': {
-            #     'download_data': False
-            # },
-            # 'DASHXBT': {
-            #     'download_data': False
-            # },
-            # 'EOSETH': {
-            #     'download_data': False
-            # },
-            # 'EOSXBT': {
-            #     'download_data': False
-            # },
-            # 'GNOETH': {
-            #     'download_data': False
-            # },
-            # 'GNOXBT': {
-            #     'download_data': False
-            # },
-            # 'USDTZUSD': {
-            #     'download_data': False
-            # },
-            # 'XETCXETH': {
-            #     'download_data': True
-            # },
-            # 'XETCXXBT': {
-            #     'download_data': True
-            # },
-            # 'XETCZEUR': {
-            #     'download_data': True
-            # },
-            # 'XETCZUSD': {
-            #     'download_data': True
-            # },
-            # 'XETHXXBT': {
-            #     'download_data': True
-            # },
-            # 'XETHXXBT.d': {
-            #     'download_data': False
-            # },
-            # 'XETHZCAD': {
-            #     'download_data': True
-            # },
-            # 'XETHZCAD.d': {
-            #     'download_data': False
-            # },
+            'BCHEUR': {
+                'download_data': True
+            },
+            'BCHUSD': {
+                'download_data': True
+            },
+            'BCHXBT': {
+                'download_data': True
+            },
+            'DASHEUR': {
+                'download_data': False
+            },
+            'DASHUSD': {
+                'download_data': False
+            },
+            'DASHXBT': {
+                'download_data': False
+            },
+            'EOSETH': {
+                'download_data': False
+            },
+            'EOSXBT': {
+                'download_data': False
+            },
+            'GNOETH': {
+                'download_data': False
+            },
+            'GNOXBT': {
+                'download_data': False
+            },
+            'USDTZUSD': {
+                'download_data': False
+            },
+            'XETCXETH': {
+                'download_data': True
+            },
+            'XETCXXBT': {
+                'download_data': True
+            },
+            'XETCZEUR': {
+                'download_data': True
+            },
+            'XETCZUSD': {
+                'download_data': True
+            },
+            'XETHXXBT': {
+                'download_data': True
+            },
+            'XETHXXBT.d': {
+                'download_data': False
+            },
+            'XETHZCAD': {
+                'download_data': True
+            },
+            'XETHZCAD.d': {
+                'download_data': False
+            },
             'XETHZEUR': {
                 'download_data': True
             },
-            # 'XETHZEUR.d': {
-            #     'download_data': False
-            # },
-            # 'XETHZGBP': {
-            #     'download_data': True
-            # },
-            # 'XETHZGBP.d': {
-            #     'download_data': False
-            # },
-            # 'XETHZJPY': {
-            #     'download_data': True
-            # },
-            # 'XETHZJPY.d': {
-            #     'download_data': False
-            # },
-            # 'XETHZUSD': {
-            #     'download_data': True
-            # },
-            # 'XETHZUSD.d': {
-            #     'download_data': False
-            # },
-            # 'XICNXETH': {
-            #     'download_data': False
-            # },
-            # 'XICNXXBT': {
-            #     'download_data': False
-            # },
-            # 'XLTCXXBT': {
-            #     'download_data': False
-            # },
-            # 'XLTCZEUR': {
-            #     'download_data': False
-            # },
-            # 'XLTCZUSD': {
-            #     'download_data': False
-            # },
-            # 'XMLNXETH': {
-            #     'download_data': False
-            # },
-            # 'XMLNXXBT': {
-            #     'download_data': False
-            # },
-            # 'XREPXETH': {
-            #     'download_data': False
-            # },
-            # 'XREPXXBT': {
-            #     'download_data': False
-            # },
-            # 'XREPZEUR': {
-            #     'download_data': False
-            # },
-            # 'XXBTZCAD': {
-            #     'download_data': True
-            # },
-            # 'XXBTZCAD.d': {
-            #     'download_data': False
-            # },
-            # 'XXBTZEUR': {
-            #     'download_data': True
-            # },
-            # 'XXBTZEUR.d': {
-            #     'download_data': False
-            # },
-            # 'XXBTZGBP': {
-            #     'download_data': True
-            # },
-            # 'XXBTZGBP.d': {
-            #     'download_data': False
-            # },
-            # 'XXBTZJPY': {
-            #     'download_data': True
-            # },
-            # 'XXBTZJPY.d': {
-            #     'download_data': False
-            # },
-            # 'XXBTZUSD': {
-            #     'download_data': True
-            # },
-            # 'XXBTZUSD.d': {
-            #     'download_data': False
-            # },
-            # 'XXDGXXBT': {
-            #     'download_data': False
-            # },
-            # 'XXLMXXBT': {
-            #     'download_data': False
-            # },
-            # 'XXMRXXBT': {
-            #     'download_data': False
-            # },
-            # 'XXMRZEUR': {
-            #     'download_data': False
-            # },
-            # 'XXMRZUSD': {
-            #     'download_data': False
-            # },
-            # 'XXRPXXBT': {
-            #     'download_data': True
-            # },
-            # 'XXRPZEUR': {
-            #     'download_data': True
-            # },
-            # 'XXRPZUSD': {
-            #     'download_data': True
-            # },
-            # 'XZECXXBT': {
-            #     'download_data': False
-            # },
-            # 'XZECZEUR': {
-            #     'download_data': False
-            # },
-            # 'XZECZUSD': {
-            #     'download_data': False
-            # }
+            'XETHZEUR.d': {
+                'download_data': False
+            },
+            'XETHZGBP': {
+                'download_data': True
+            },
+            'XETHZGBP.d': {
+                'download_data': False
+            },
+            'XETHZJPY': {
+                'download_data': True
+            },
+            'XETHZJPY.d': {
+                'download_data': False
+            },
+            'XETHZUSD': {
+                'download_data': True
+            },
+            'XETHZUSD.d': {
+                'download_data': False
+            },
+            'XICNXETH': {
+                'download_data': False
+            },
+            'XICNXXBT': {
+                'download_data': False
+            },
+            'XLTCXXBT': {
+                'download_data': False
+            },
+            'XLTCZEUR': {
+                'download_data': False
+            },
+            'XLTCZUSD': {
+                'download_data': False
+            },
+            'XMLNXETH': {
+                'download_data': False
+            },
+            'XMLNXXBT': {
+                'download_data': False
+            },
+            'XREPXETH': {
+                'download_data': False
+            },
+            'XREPXXBT': {
+                'download_data': False
+            },
+            'XREPZEUR': {
+                'download_data': False
+            },
+            'XXBTZCAD': {
+                'download_data': True
+            },
+            'XXBTZCAD.d': {
+                'download_data': False
+            },
+            'XXBTZEUR': {
+                'download_data': True
+            },
+            'XXBTZEUR.d': {
+                'download_data': False
+            },
+            'XXBTZGBP': {
+                'download_data': True
+            },
+            'XXBTZGBP.d': {
+                'download_data': False
+            },
+            'XXBTZJPY': {
+                'download_data': True
+            },
+            'XXBTZJPY.d': {
+                'download_data': False
+            },
+            'XXBTZUSD': {
+                'download_data': True
+            },
+            'XXBTZUSD.d': {
+                'download_data': False
+            },
+            'XXDGXXBT': {
+                'download_data': False
+            },
+            'XXLMXXBT': {
+                'download_data': False
+            },
+            'XXMRXXBT': {
+                'download_data': False
+            },
+            'XXMRZEUR': {
+                'download_data': False
+            },
+            'XXMRZUSD': {
+                'download_data': False
+            },
+            'XXRPXXBT': {
+                'download_data': True
+            },
+            'XXRPZEUR': {
+                'download_data': True
+            },
+            'XXRPZUSD': {
+                'download_data': True
+            },
+            'XZECXXBT': {
+                'download_data': False
+            },
+            'XZECZEUR': {
+                'download_data': False
+            },
+            'XZECZUSD': {
+                'download_data': False
+            }
         }
 
     def update_ohlc_db(self):
